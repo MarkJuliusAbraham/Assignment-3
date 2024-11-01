44ed4d25819420a3b8e1c9a4d349210b197a5030
Riyasat Zaman <riyasat@ualberta.ca>
2024-10-31 20:31:30 -0600

    updated policy_moves

--- assignment3/a3.py
+++ assignment3/a3.py
@@ -1,41 +1,40 @@
 # CMPUT 455 Assignment 3 starter code
 # Implement the specified commands to complete the assignment
-# Full assignment specification here: https://webdocs.cs.ualberta.ca/~mmueller/courses/cmput455/assignments/a3.html
 
 import sys
 import random
 import os
+
 
 class CommandInterface:
 
     def __init__(self):
         # Define the string to function command mapping
         self.command_dict = {
-            "help" : self.help,
-            "game" : self.game,
-            "show" : self.show,
-            "play" : self.play,
-            "legal" : self.legal,
-            "genmove" : self.genmove,
-            "winner" : self.winner,
-            "loadpatterns" : self.loadpatterns,
-            "policy_moves" : self.policy_moves
+            "help": self.help,
+            "game": self.game,
+            "show": self.show,
+            "play": self.play,
+            "legal": self.legal,
+            "genmove": self.genmove,
+            "winner": self.winner,
+            "loadpatterns": self.loadpatterns,
+            "policy_moves": self.policy_moves
         }
         self.board = [[None]]
         self.player = 1
         self.patterns = {}
-    
-    #===============================================================================================
-    # VVVVVVVVVV START of PREDEFINED FUNCTIONS. DO NOT MODIFY. VVVVVVVVVV
-    #===============================================================================================
 
-    # Convert a raw string to a command and a list of arguments
+    # ===============================================================================================
+    # VVVVVVVVVV START of PREDEFINED FUNCTIONS. DO NOT MODIFY. VVVVVVVVVV
+    # ===============================================================================================
+
     def process_command(self, str):
         str = str.lower().strip()
         command = str.split(" ")[0]
         args = [x for x in str.split(" ")[1:] if len(x) > 0]
         if command not in self.command_dict:
-            print("? Uknown command.\nType 'help' to list known commands.", file=sys.stderr)
+            print("? Unknown command.\nType 'help' to list known commands.", file=sys.stderr)
             print("= -1\n")
             return False
         try:
@@ -45,10 +44,7 @@ class CommandInterface:
             print(e, file=sys.stderr)
             print("= -1\n")
             return False
-        
-    # Will continuously receive and execute commands
-    # Commands should return True on success, and False on failure
-    # Every command will print '= 1' or '= -1' at the end of execution to indicate success or failure respectively
+
     def main_loop(self):
         while True:
             str = input()
@@ -58,13 +54,11 @@ class CommandInterface:
             if self.process_command(str):
                 print("= 1\n")
 
-    # Will make sure there are enough arguments, and that they are valid numbers
-    # Not necessary for commands without arguments
     def arg_check(self, args, template):
         converted_args = []
         if len(args) < len(template.split(" ")):
             print("Not enough arguments.\nExpected arguments:", template, file=sys.stderr)
-            print("Recieved arguments: ", end="", file=sys.stderr)
+            print("Received arguments: ", end="", file=sys.stderr)
             for a in args:
                 print(a, end=" ", file=sys.stderr)
             print(file=sys.stderr)
@@ -73,12 +67,12 @@ class CommandInterface:
             try:
                 converted_args.append(int(arg))
             except ValueError:
-                print("Argument '" + arg + "' cannot be interpreted as a number.\nExpected arguments:", template, file=sys.stderr)
+                print("Argument '" + arg + "' cannot be interpreted as a number.\nExpected arguments:", template,
+                      file=sys.stderr)
                 return False
         args = converted_args
         return True
 
-    # List available commands
     def help(self, args):
         for command in self.command_dict:
             if command != "help":
@@ -86,13 +80,13 @@ class CommandInterface:
         print("exit")
         return True
 
-    #===============================================================================================
+    # ===============================================================================================
     # ɅɅɅɅɅɅɅɅɅɅ END OF PREDEFINED FUNCTIONS. ɅɅɅɅɅɅɅɅɅɅ
-    #===============================================================================================
+    # ===============================================================================================
 
-    #===============================================================================================
+    # ===============================================================================================
     # VVVVVVVVVV START OF ASSIGNMENT 3 FUNCTIONS. ADD/REMOVE/MODIFY AS NEEDED. VVVVVVVV
-    #===============================================================================================
+    # ===============================================================================================
 
     def game(self, args):
         if not self.arg_check(args, "n m"):
@@ -101,13 +95,13 @@ class CommandInterface:
         if n < 0 or m < 0:
             print("Invalid board size:", n, m, file=sys.stderr)
             return False
-        
+
         self.board = []
         for i in range(m):
-            self.board.append([None]*n)
+            self.board.append([None] * n)
         self.player = 1
         return True
-    
+
     def show(self, args):
         for row in self.board:
             for x in row:
@@ -115,13 +109,13 @@ class CommandInterface:
                     print(".", end="")
                 else:
                     print(x, end="")
-            print()                    
+            print()
         return True
 
     def is_legal_reason(self, x, y, num):
         if self.board[y][x] is not None:
             return False, "occupied"
-        
+
         consecutive = 0
         count = 0
         self.board[y][x] = num
@@ -135,7 +129,7 @@ class CommandInterface:
             else:
                 consecutive = 0
         too_many = count > len(self.board) // 2 + len(self.board) % 2
-        
+
         consecutive = 0
         count = 0
         for col in range(len(self.board[0])):
@@ -153,11 +147,11 @@ class CommandInterface:
 
         self.board[y][x] = None
         return True, ""
-    
+
     def is_legal(self, x, y, num):
         if self.board[y][x] is not None:
             return False
-        
+
         consecutive = 0
         count = 0
         self.board[y][x] = num
@@ -173,7 +167,7 @@ class CommandInterface:
         if count > len(self.board) // 2 + len(self.board) % 2:
             self.board[y][x] = None
             return False
-        
+
         consecutive = 0
         count = 0
         for col in range(len(self.board[0])):
@@ -191,15 +185,14 @@ class CommandInterface:
 
         self.board[y][x] = None
         return True
-    
+
     def valid_move(self, x, y, num):
-        return  x >= 0 and x < len(self.board[0]) and\
-                y >= 0 and y < len(self.board) and\
-                (num == 0 or num == 1) and\
-                self.is_legal(x, y, num)
+        return x >= 0 and x < len(self.board[0]) and \
+            y >= 0 and y < len(self.board) and \
+            (num == 0 or num == 1) and \
+            self.is_legal(x, y, num)
 
     def play(self, args):
-        err = ""
         if len(args) != 3:
             print("= illegal move: " + " ".join(args) + " wrong number of arguments\n")
             return False
@@ -209,7 +202,7 @@ class CommandInterface:
         except ValueError:
             print("= illegal move: " + " ".join(args) + " wrong coordinate\n")
             return False
-        if  x < 0 or x >= len(self.board[0]) or y < 0 or y >= len(self.board):
+        if x < 0 or x >= len(self.board[0]) or y < 0 or y >= len(self.board):
             print("= illegal move: " + " ".join(args) + " wrong coordinate\n")
             return False
         if args[2] != '0' and args[2] != '1':
@@ -221,12 +214,9 @@ class CommandInterface:
             print("= illegal move: " + " ".join(args) + " " + reason + "\n")
             return False
         self.board[y][x] = num
-        if self.player == 1:
-            self.player = 2
-        else:
-            self.player = 1
+        self.player = 2 if self.player == 1 else 1
         return True
-    
+
     def legal(self, args):
         if not self.arg_check(args, "x y number"):
             return False
@@ -236,7 +226,7 @@ class CommandInterface:
         else:
             print("no")
         return True
-    
+
     def get_legal_moves(self):
         moves = []
         for y in range(len(self.board)):
@@ -251,121 +241,83 @@ class CommandInterface:
         if len(moves) == 0:
             print("resign")
         else:
-            rand_move = moves[random.randint(0, len(moves)-1)]
+            rand_move = moves[random.randint(0, len(moves) - 1)]
             self.play(rand_move)
             print(" ".join(rand_move))
         return True
-    
+
     def winner(self, args):
         if len(self.get_legal_moves()) == 0:
-            if self.player == 1:
-                print(2)
-            else:
-                print(1)
+            print(2 if self.player == 1 else 1)
         else:
             print("unfinished")
         return True
-    
-    # new function to be implemented for assignment 3
+
+    # New function for loading patterns
     def loadpatterns(self, args):
-        # raise NotImplementedError("This command is not yet implemented.")
-        self.loadpatternsEY(args)
-        # script_directory = os.path.dirname(os.path.abspath(__file__))
-        # with open(script_directory+"/twopattern.txt",'r') as file:
-        #     print(script_directory+"/twopattern.txt")
-        return True
-    def loadpatternsEY (self, args) :
-        script_directory = os.path.dirname(os.path.abspath(__file__))
-        filename = script_directory+"/"+args[0]
         self.patterns.clear()
-        with open(filename, 'r') as file:
-            for line in file:
-                line = line.strip()
-                if not line or line.startswith ("#"):
-                    continue # Skip empty lines and comments
-                parts = line.split()
-                pattern = parts[0]
-                move = int (parts [1])
-                weight = int(parts [2])
-                if pattern not in self.patterns:
-                    self.patterns[pattern] = {}
-                self.patterns[pattern][move]= weight
-        print(self.patterns)
+        try:
+            with open(args[0], 'r') as file:
+                for line in file:
+                    line = line.strip()
+                    if not line or line.startswith("#"):
+                        continue  # Skip empty lines and comments
+                    pattern, move, weight = line.split()
+                    move, weight = int(move), int(weight)
+                    if pattern not in self.patterns:
+                        self.patterns[pattern] = {}
+                    self.patterns[pattern][move] = weight
+        except FileNotFoundError:
+            print("Pattern file not found", file=sys.stderr)
+            return False
         return True
-    
-    # new function to be implemented for assignment 3
+
+    # Function to calculate policy moves with probabilities
     def policy_moves(self, args):
-        
-        self.policy_movesMark(args)
+        legal_moves = self.get_legal_moves()
+        move_weights = []
+        total_weight = 0
+
+        for move in legal_moves:
+            x, y, num = int(move[0]), int(move[1]), int(move[2])
+
+            # Generate row and column patterns
+            row_pattern = self.make_pattern(x, y, axis=0)  # Row pattern
+            col_pattern = self.make_pattern(x, y, axis=1)  # Column pattern
+
+            # Get weights for row and column patterns, or default weight of 10
+            row_weight = self.patterns.get(row_pattern, {}).get(num, 10)
+            col_weight = self.patterns.get(col_pattern, {}).get(num, 10)
+
+            # Calculate the total weight for the move
+            move_weight = row_weight + col_weight
+            move_weights.append((move, move_weight))
+            total_weight += move_weight
+
+        # Generate output with probabilities
+        result = "= "
+        for move, weight in sorted(move_weights):
+            probability = round(weight / total_weight, 3)
+            result += f"{' '.join(move)} {probability} "
+        print(result.strip())
         return True
 
-    def policy_movesEY(self, args):
-        print(self.get_legal_moves())
-
-    def policy_movesMark(self, args):
-
-        legal_moves = self.get_legal_moves()
-        for legal_move in legal_moves:
-            x_axis = int(legal_move[0])
-            y_axis = int(legal_move[1])  
-
-            self.make_pattern(x_axis,y_axis,0)
-                #record the pattern
-            self.make_pattern(x_axis,y_axis,0)
-                #record the pattern
-
-            #flipping
-            #check if pattern and its axis-counterparts are in self.patterns
-    
-
-
-    def make_pattern(self,x_axis,y_axis,boolean):
-        """
-            if boolean is 0, return the row pattern
-            if boolean is 1, return the column pattern
-        """
-        if(boolean == 1):
-            temp = x_axis
-            x_axis = y_axis
-            y_axis = temp
-
-        five_pattern = ""
-
-        for i in range(2):
-            current_x = x_axis-2+i
-            if((current_x)<0):
-                five_pattern = five_pattern+"X"
+    def make_pattern(self, x, y, axis=0):
+        pattern = ""
+        for i in range(-2, 3):
+            nx, ny = (x + i, y) if axis == 0 else (x, y + i)
+            if nx < 0 or ny < 0 or nx >= len(self.board[0]) or ny >= len(self.board):
+                pattern += "X"  # Off the board
             else:
-                
-                if(boolean):
-                    item = self.board[y_axis][current_x]
-                else:
-                    item = self.board[current_x][y_axis]
+                cell = self.board[ny][nx]
+                pattern += str(cell) if cell is not None else "."
+        return pattern
 
-                if (item == None):
-                    item = '.'
-                five_pattern = five_pattern+str(item)
-        five_pattern = five_pattern + "."
 
-        for i in range(2):
-            current_x = x_axis+(i+1)
-            if((current_x)>=len(self.board[0])):
-                five_pattern = five_pattern+"X"
-            else:
-                if(boolean):
-                    item = self.board[y_axis][current_x]
-                else:
-                    item = self.board[current_x][y_axis]
+# ===============================================================================================
+# ɅɅɅɅɅɅɅɅɅɅ END OF ASSIGNMENT 3 FUNCTIONS. ɅɅɅɅɅɅɅɅɅɅ
+# ===============================================================================================
 
-                if (item == None):
-                    item = '.'
-                five_pattern = five_pattern+str(item)
-
-        return five_pattern
-    #======================================================================================s=========
-    # ɅɅɅɅɅɅɅɅɅɅ END OF ASSIGNMENT 3 FUNCTIONS. ɅɅɅɅɅɅɅɅɅɅ
-    #===============================================================================================
-    
 if __name__ == "__main__":
     interface = CommandInterface()
     interface.main_loop()
